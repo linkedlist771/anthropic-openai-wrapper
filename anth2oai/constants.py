8315eac570fbf3d101f631c72dece5aac575e95a
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2026-01-17 22:02:23 +0800

    Update constants.py

--- anth2oai/constants.py
+++ anth2oai/constants.py
@@ -2,19 +2,9 @@ DEFAULT_THIKING_CONFIG = {"type": "enabled", "budget_tokens": 6000}
 
 
 DEFAULT_HTTP_CLIENT_HEADERS = {
-    "accept": "application/json",
-    "anthropic-beta": "claude-code-20250219,context-1m-2025-08-07,interleaved-thinking-2025-05-14,fine-grained-tool-streaming-2025-05-14,tmp-preserve-thinking-2025-10-01",
-    "anthropic-dangerous-direct-browser-access": "true",
-    "anthropic-version": "2023-06-01",
-    "content-type": "application/json",
-    "host": "api.anthropic.com",
-    "user-agent": "claude-cli/1.0.25 (external, cli)",
-    "x-app": "cli",
-    "x-stainless-arch": "x64",
-    "x-stainless-helper-method": "stream",
-    "x-stainless-lang": "js",
-    "x-stainless-os": "Linux",
-    "x-stainless-package-version": "0.52.0",
-    "x-stainless-retry-count": "0",
-    "x-stainless-runtime": "node",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Accept": "application/json, text/event-stream",
+    "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
 }
